36ece59248b03ef465f89595cb0365247ca7253a
Juliana Bernardes Pereira <julianabp96@gmail.com>
2021-02-15 10:12:08 +0000

    initial commit

---
+++ contextapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BookList from './components/BookList'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <BookList></BookList>
+    </div>
+  );
+}
+
+export default App;
