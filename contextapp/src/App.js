b2b42c53e2fecb730d3c3f7d87f1890a28fcf70c
Juliana Bernardes Pereira <julianabp96@gmail.com>
2021-02-16 10:08:18 +0000

    adding  context and provider

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import BookList from './components/BookList'
 import Navbar from './components/Navbar'
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <BookList></BookList>
+      <ThemeContextProvider>
+        <Navbar></Navbar>
+        <BookList></BookList>
+      </ThemeContextProvider>
     </div>
   );
 }
