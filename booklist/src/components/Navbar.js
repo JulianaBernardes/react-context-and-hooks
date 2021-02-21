444b96bd26d1e02e321c751db49f6075edbc41f0
Juliana Bernardes Pereira <julianabp96@gmail.com>
2021-02-21 12:04:27 +0000

    booklist components and context

--- booklist/src/components/Navbar.js
+++ booklist/src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
-import {BookContext} from '../context/BookContext'
+import { BookContext } from '../context/BookContext'
 
 const Navbar = () => {
-    const {books} = useContext(BookContext)
+    const { books } = useContext(BookContext)
     return (
         <div className="navbar">
             <h1>Reading List</h1>
